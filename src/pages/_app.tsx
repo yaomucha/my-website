8ed210f8872d5c7102c6721008e85bc1db2a05ad
guo jiao <1538678381@qq.com>
2023-03-15 18:19:39 +0800

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+
+// 顶级 React 组件，它包装了应用程序中的所有页面。
+// https://nextjs.org/docs/advanced-features/custom-app
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
