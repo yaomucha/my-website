155a6db2f85616b673db04414ff984e9af557a20
guo jiao <1538678381@qq.com>
2023-05-10 17:35:56 +0800

    添加404页面（未完成

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
-
-import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 import "inter-ui/inter.css";
 
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,3 +1,30 @@
+import { useEffect, useState } from "react";
+
 export default function Custom404() {
-    return <h1>404 - Page Not Found</h1>;
-  }
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("https://api.isoyu.com/gy/data.js").then(res => {
+      return res.text()
+    }).then(res => {
+      const _data = res.split("var jsondata=")[1]
+      setData(JSON.parse(_data).data)
+    })
+
+  }, [])
+
+  return (<>
+    {
+      data.map(item => {
+        return (
+          <div>
+            <div>
+              <img src={item.child_pic} /> 
+            </div>
+            <div>{item.name}</div>
+          </div>
+        )
+      })
+    }
+  </>);
+}
